2c4c99d11df2272135e2b45ebff6a6f0fc851515
sakuun_aah <91127128+amineaith3@users.noreply.github.com>
2026-02-09 20:17:23 +0100

    Update RestaurantCustomers.cpp

--- SortingAndSearching/RestaurantCustomers.cpp
+++ SortingAndSearching/RestaurantCustomers.cpp
@@ -9,25 +9,19 @@ typedef set<plli> splli;
 int main() {
     int n;
     cin >> n;
-
     splli customers;
-
     for (int i = 0; i < n; i++) {
         ll arrival, departure;
         cin >> arrival >> departure;
         customers.insert({arrival, +1});
         customers.insert({departure, -1});
     }
-
     int ans = 0;
     int max_customers = 0;
-
     for (auto c : customers) {
         ans += c.second;
         max_customers = max(max_customers, ans);
     }
-
     cout << max_customers << ln;
-
     return 0;
 }
